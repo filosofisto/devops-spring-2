884d55cc479dc6c5b29890eccfe64776798f89c3
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-05-28 18:47:02 +0200

    Build unstable

--- src/test/java/com/archsoft/Spring2ApplicationTests.java
+++ src/test/java/com/archsoft/Spring2ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.archsoft;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class Spring2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("Build unstable");
 	}
 
 }
